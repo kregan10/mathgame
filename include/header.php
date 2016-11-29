bd37c6314d4b877c6bdd39356a03fbff86d5a6db
kregan10 <kregan89@gmail.com>
2016-11-29 12:56:31 -0800

    adding fixed files

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link href = "style/bootstrap.min.css" rel = "stylesheet"/>
+      <title>Number Guessing game!</title>
+      <link rel="stylesheet" href="style/coincount.css" type="text/css" />
+      <meta charset="utf-8" />
+    </head>
+    <body>
+      <div class="container">
+        <div class="jumbotron">
+          <header class="content">
+              <h1>Math Game</h1>
+              <h3>Guess the right numbers to get the best score!</h3>
+              <br />
+          </header>
+      </div>
+    </div>
