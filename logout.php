32703d9b6a264aff9ae3012f9ff962e282105b2d
kregan10 <kregan89@gmail.com>
2016-11-29 16:26:14 -0800

    fixed logout

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 session_destroy();
-$url = "http://$_SERVER[HTTP_HOST]/Regan_Kerry_php/login.php";
+$url = "http://$_SERVER[HTTP_HOST]/login.php";
 header("Location: $url"); // must be absolute
 exit;
 ?>
